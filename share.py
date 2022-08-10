15051552032abf5639849afbb125da0243e5b203
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2022-08-10 18:33:34 +0700

    Add files via upload

---
+++ share.py
@@ -0,0 +1,41 @@
+import os, re, requests, json, bs4
+from bs4 import BeautifulSoup as parser
+ses=requests.Session()
+
+def login():
+	cookie = input(" masukan cookie : ")
+	try:
+		data = ses.get("https://business.facebook.com/business_locations", headers = {"user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36","referer": "https://www.facebook.com/","host": "business.facebook.com","origin": "https://business.facebook.com","upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","content-type":"text/html; charset=utf-8"}, cookies = {"cookie":cookie})
+		find_token = re.search("(EAAG\w+)", data.text)
+		open("token.txt", "w").write(find_token.group(1))
+		open("cookie.txt", "w").write(cookie)
+		menu()
+	except:
+		os.system("rm token.txt cookie.txt")
+		exit(" login gagal kemungkinan cookie invalid atau tumbal mati")
+		
+	
+def menu():
+	try:
+		token = open("token.txt","r").read()
+		cok = open("cookie.txt","r").read()
+		cookie = {"cookie":cok}
+	except:
+		login()
+	idt = input(" masukan link : ")
+	limit = int(input(" masukan limit : "))
+	try:
+		n = 0
+		header = {"authority":"graph.facebook.com","cache-control":"max-age=0","sec-ch-ua-mobile":"?0","user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.66 Safari/537.36"}
+		for x in range(limit):
+			n+=1
+			post = ses.post(f"https://graph.facebook.com/v13.0/me/feed?link={idt}&published=0&access_token={token}",headers=header, cookies=cookie).text
+			data = json.loads(post)
+			if "id" in post:
+				print(f" {n}. berhasil membagikan {data['id']}")
+			else:
+				exit(" gagal membagikan, kemungkinan token invalid")
+	except:
+		exit(" gagal membagikan, kemungkinan token invalid")
+		
+menu()
