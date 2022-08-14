66544baa4e1e257ed95ecf98f4a75f979e7dfa62
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2022-08-15 00:11:54 +0700

    Update build.py

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ main_file = "share.c"
 
 def install(main_file):
 	nama = main_file.split(".")[0]
-	setup(name = main_file,ext_modules = [Extension(main, [main_file])],script_args = ['build_ext', '--inplace', '--force', '-j 5'])
+	setup(name = main_file,ext_modules = [Extension(nama, [main_file])],script_args = ['build_ext', '--inplace', '--force', '-j 5'])
 	if os.path.exists(main_file):
 		try: shutil.rmtree('build/')
 		except:0
