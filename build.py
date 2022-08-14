a9fad1150465136ae877df73251eefa9b6e3dd76
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2022-08-15 00:10:33 +0700

    Create build.py

---
+++ build.py
@@ -0,0 +1,18 @@
+import os,sys,shutil
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+main_file = "share.c"
+
+def install(main_file):
+	nama = main_file.split(".")[0]
+	setup(name = main_file,ext_modules = [Extension(main, [main_file])],script_args = ['build_ext', '--inplace', '--force', '-j 5'])
+	if os.path.exists(main_file):
+		try: shutil.rmtree('build/')
+		except:0
+		try: os.remove(main_file)
+		except:0
+	else:
+		print("ketik 'python -m pip install -r bahan.txt")
+
+install(main_file)
